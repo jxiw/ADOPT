b38b6390fb1521d63556b25e5fc954e6a151d9d4
Mitchell Gray <meg346@cornell.edu>
2023-03-30 23:19:28 -0400

    Fixed totalVisualization

--- src/totalVisualization/HelloFX.java
+++ src/totalVisualization/HelloFX.java
@@ -5,9 +5,15 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -74,13 +80,24 @@ public class HelloFX extends Application {
 			selectedProgram = "ThreadPieChart";
 		});
 		VBox rightButtons = new VBox();
+		leftButtons.setTranslateY(42);
+		leftButtons.setSpacing(10);
+		rightButtons.setTranslateY(42);
+		rightButtons.setSpacing(10);
 		Button fileOpener = new Button("Select Log File");
+		ObservableList<String> options = FXCollections.observableArrayList("3-clique", "4-clique", "5-clique",
+				"3-cycle", "4-cycle", "5-cycle");
+		ComboBox combo = new ComboBox(options);
+		combo.setPromptText("Premade Queries");
+		combo.setPrefWidth(350);
+		combo.setPrefHeight(75);
 		Text fileName = new Text("Not Selected");
 		fileName.setFont(new Font(32));
 		fileOpener.setFont(new Font(36));
 		fileOpener.setPrefWidth(350);
 		fileOpener.setPrefHeight(75);
 		rightButtons.getChildren().add(fileOpener);
+		rightButtons.getChildren().add(combo);
 		fileOpener.setOnMouseClicked(e -> {
 			FileChooser fileChooser = new FileChooser();
 			fileChooser.setTitle("Open Log File");
@@ -92,11 +109,25 @@ public class HelloFX extends Application {
 			fileName.setText(temp);
 		});
 
-		HBox center = new HBox();
+		VBox center = new VBox();
+		TextArea queryArea = new TextArea();
+		queryArea.setWrapText(true);
+		ScrollPane s = new ScrollPane();
+		s.setContent(queryArea);
+		s.setHbarPolicy(ScrollBarPolicy.NEVER);
+		s.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+		queryArea.setPromptText("Enter Custom Queries Here");
+		queryArea.setFont(new Font(36));
+		HBox centerText = new HBox();
+		center.getChildren().add(centerText);
+		center.getChildren().add(queryArea);
+		queryArea.setStyle("-fx-focus-color: black;");
+//		queryArea.setFocusTraversable(false);
+		queryArea.setPrefHeight(800);
 		Text filePrompt = new Text("Log File: ");
 		filePrompt.setFont(new Font(32));
-		center.getChildren().addAll(filePrompt, fileName);
-		center.setAlignment(Pos.TOP_CENTER);
+		centerText.getChildren().addAll(filePrompt, fileName);
+		centerText.setAlignment(Pos.TOP_CENTER);
 
 		leftButtons.getChildren().addAll(button, button2, button3, button4);
 
@@ -166,6 +197,7 @@ public class HelloFX extends Application {
 		pane.setCenter(center);
 		pane.setBottom(bottomStart);
 		Scene scene = new Scene(pane);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setWidth(1920);
 		primaryStage.setHeight(1000);
 		primaryStage.setScene(scene);
