72146d46735668677d272a800bb9100ff1a564f6
Junxiong Wang <chuangzhetianxia@gmail.com>
2023-04-09 21:33:44 -0400

    fix tpch q11&q16

--- src/joining/ParallelJoinProcessor.java
+++ src/joining/ParallelJoinProcessor.java
@@ -27,6 +27,7 @@ import query.QueryInfo;
 import statistics.JoinStats;
 import statistics.PostStats;
 import statistics.PreStats;
+import util.CartesianProduct;
 import util.MergedList;
 
 import java.util.*;
@@ -34,6 +35,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * This variant of the join processor parallelize
@@ -85,7 +87,8 @@ public class ParallelJoinProcessor {
             int skinnerJoinCard = CatalogManager.getCardinality(targetRelName);
             JoinStats.skinnerJoinCards.add(skinnerJoinCard);
             System.out.println("Join card: " + skinnerJoinCard + "\tJoin time:" + Arrays.toString(JoinStats.subExeTime.toArray()));
-        } else {
+        } else if (query.equiJoinPreds.size() > 0) {
+            // Join condition, apply leapfrog triejoin
             Set<ResultTuple> resultTuples = new HashSet<>();
             long startJoinMillis = System.currentTimeMillis();
 //            long mergeMillis = 0;
@@ -120,7 +123,7 @@ public class ParallelJoinProcessor {
                 // number of non equality join predicates
                 int nrNonEquivalentPredict = query.nonEquiJoinNodes.size();
                 List<ExpressionInfo> nonEquiExpressions = query.nonEquiJoinPreds;
-                List<NonEquiNode> nonEquiNodes = query.nonEquiJoinNodes;
+//                List<NonEquiNode> nonEquiNodes = query.nonEquiJoinNodes;
                 // map non equality predicate id to table id
                 HashMap<Integer, Integer> nonEquiTablesToAliasIndex = new HashMap<>();
                 HashMap<Integer, Integer> nonEquiCards = new HashMap<>();
@@ -144,6 +147,8 @@ public class ParallelJoinProcessor {
                     validateRowIds.add(rid);
                 }
 
+                System.out.println("validate size:" + results.size());
+
                 for (int nonEquiPredictId = 0; nonEquiPredictId < nrNonEquivalentPredict; nonEquiPredictId++) {
                     int processTableId = nonEquiTablesToAliasIndex.get(nonEquiPredictId);
                     int cardinality = nonEquiCards.get(nonEquiPredictId);
@@ -151,14 +156,23 @@ public class ParallelJoinProcessor {
                     Set<Integer> currentValidateRowIds = new HashSet<>();
                     for (int rid : validateRowIds) {
                         int[] result = results.get(rid);
-                        result[processTableId] = 0;
-                        while (result[processTableId] < cardinality) {
+                        if (result[processTableId] == -1) {
+                            // non equality condition is not mentioned before, should be exist or non-exist in subqueries
+                            result[processTableId] = 0;
+                            while (result[processTableId] < cardinality) {
+                                if (nonEquiNode.evaluate(result, processTableId, cardinality)) {
+                                    // satisfy condition
+                                    currentValidateRowIds.add(rid);
+                                    break;
+                                } else {
+                                    result[processTableId]++;
+                                }
+                            }
+                        } else {
+                            // test directly
                             if (nonEquiNode.evaluate(result, processTableId, cardinality)) {
                                 // satisfy condition
                                 currentValidateRowIds.add(rid);
-                                break;
-                            } else {
-                                result[processTableId]++;
                             }
                         }
                     }
@@ -171,10 +185,35 @@ public class ParallelJoinProcessor {
                     resultTuples.add(new ResultTuple(results.get(validateRowId)));
                 }
 
-            } else {
+            } else if (results.size() > 0) {
+                boolean applyCartesianProduct = false;
+                int cartesianProductTableIndex = -1;
+                for (int tid = 0; tid < results.get(0).length; tid++) {
+                    int rid = results.get(0)[tid];
+                    if (rid == -1) {
+                        applyCartesianProduct = true;
+                        cartesianProductTableIndex = tid;
+                        break;
+                    }
+                }
+                if (applyCartesianProduct) {
+                    // add more results here
+                    List<int[]> cardResults = new ArrayList<>();
+                    String alias = query.aliases[cartesianProductTableIndex];
+                    String table = context.aliasToFiltered.get(alias);
+                    int card = CatalogManager.getCardinality(table);
+                    for (int[] result : results) {
+                        result[cartesianProductTableIndex] = 0;
+                        for (int i = 1; i < card; i++) {
+                            int[] newResult = Arrays.copyOf(result, result.length);
+                            newResult[cartesianProductTableIndex] = i;
+                            cardResults.add(newResult);
+                        }
+                    }
+                    results.add(cardResults);
+                }
                 resultTuples = results.stream().map(ResultTuple::new).collect(Collectors.toSet());
             }
-
 
             long endJoinMillis = System.currentTimeMillis();
             System.out.println("Finish Parallel Join!");
@@ -183,7 +222,7 @@ public class ParallelJoinProcessor {
 //            int median = GeneralConfig.TEST_CASE / 2;
 //            long[] subExe = subExes.stream().mapToLong(exe->exe).toArray();
 //            Arrays.sort(subExe);
-            JoinStats.subExeTime.add((endJoinMillis - startJoinMillis));
+//            JoinStats.subExeTime.add((endJoinMillis - startJoinMillis));
 
             long materializeStart = System.currentTimeMillis();
             // Materialize result table
@@ -208,11 +247,72 @@ public class ParallelJoinProcessor {
             JoinStats.skinnerJoinCards.add(skinnerJoinCard);
             System.out.println("Join card: " + skinnerJoinCard + "\tJoin time:" + Arrays.toString(JoinStats.subExeTime.toArray()));
 
+        } else if (query.nonEquiJoinPreds.size() > 0) {
+            // only contain non equality predicates
+            Set<ResultTuple> resultTuples = new HashSet<>();
+            // only support two tables here
+            int cardinality1 = CatalogManager.getCardinality(context.aliasToFiltered.get(query.aliases[0]));
+            int cardinality2 = CatalogManager.getCardinality(context.aliasToFiltered.get(query.aliases[1]));
+            int[] rowIds = new int[]{0, 0};
+            for (int i = 0; i < cardinality1; i++) {
+                rowIds[0] = i;
+                rowIds[1] = 0;
+                while (rowIds[1] < cardinality2) {
+                    if (query.nonEquiJoinNodes.get(0).evaluate(rowIds, 1, cardinality2)) {
+                        // satisfy condition
+                        resultTuples.add(new ResultTuple(rowIds));
+                    }
+                    rowIds[1]++;
+                }
+            }
+
+
+//            List<List<Integer>> resultLists = new ArrayList<>();
+//            int tid = 0;
+//            int cardinality = 0;
+//            for (; tid < query.nrJoined; tid++) {
+//                String alias = query.aliases[tid];
+//                String table = context.aliasToFiltered.get(alias);
+//                cardinality = CatalogManager.getCardinality(table);
+//                resultLists.add(IntStream.range(0, cardinality).boxed().collect(Collectors.toList()));
+//            }
+//            List<List<Integer>> finalResults = CartesianProduct.constructCombinations(resultLists);
+//            for (List<Integer> finalResult : finalResults) {
+//                int[] result = finalResult.stream().mapToInt(Integer::intValue).toArray();
+//                boolean validate = true;
+//                for (NonEquiNode nonEquiNode : query.nonEquiJoinNodes) {
+//                    if (!nonEquiNode.evaluate(result, tid - 1, cardinality)) {
+//                        validate = false;
+//                        break;
+//                    }
+//                }
+//                if (validate) {
+//                    resultTuples.add(new ResultTuple(result));
+//                }
+//            }
+
+            long materializeStart = System.currentTimeMillis();
+            String targetRelName = NamingConfig.JOINED_NAME;
+            Materialize.execute(resultTuples, query.aliasToIndex,
+                    query.colsForPostProcessing,
+                    context.columnMapping, targetRelName);
+            context.columnMapping.clear();
+            for (ColumnRef postCol : query.colsForPostProcessing) {
+                String newColName = postCol.aliasName + "." + postCol.columnName;
+                ColumnRef newRef = new ColumnRef(targetRelName, newColName);
+                context.columnMapping.put(postCol, newRef);
+            }
+            long materializeEnd = System.currentTimeMillis();
+            JoinStats.subMateriazed.add(materializeEnd - materializeStart);
+            // Store number of join result tuples
+            int skinnerJoinCard = resultTuples.size();
+            JoinStats.skinnerJoinCards.add(skinnerJoinCard);
+            System.out.println("Join card: " + skinnerJoinCard + "\tJoin time:" + Arrays.toString(JoinStats.subExeTime.toArray()));
         }
         // Measure execution time for join phase
         JoinStats.joinMillis = System.currentTimeMillis() - startMillis;
-
     }
+
 
     /**
      * Print out log entry if the maximal number of log
